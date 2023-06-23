83b736248c71e29b2001e866d7800c71e6673117
MForystek <micforystek@gmail.com>
2023-06-23 20:18:55 +0200

    BSS-11 - Added script that fills the database with mock data. Docker compose splitted to prod and dev versions (prod is default)

--- api/src/Models/Product.cs
+++ api/src/Models/Product.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace beauty_supplies_store.Models;
 
+[Table("products")]
 public class Product
 {
+    [Column("id"), Key]
     public int ID { get; set; }
+    [Column("name"), Required]
     public string? Name { get; set; }
+    [Column("producer"), Required]
+    public string? Producer { get; set; }
+    [Column("price"), Required]
     public int Price { get; set; }
-    public DateTime ExpiryDate { get; set; }
+    [Column("expiry_date", TypeName = "DATE")]
+    public DateTime? ExpiryDate { get; set; }
 }
