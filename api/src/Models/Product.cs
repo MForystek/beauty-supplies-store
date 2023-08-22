1f96e755cb2957a625894f6ad85df315b4da4c7b
MForystek <micforystek@gmail.com>
2023-08-22 17:22:35 +0200

    BSS-16 - Added product catalog, in api changed beauty-supplies-store to api for consistency, updated README.md and applications ports (#9)

--- api/src/Models/Product.cs
+++ api/src/Models/Product.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-namespace beauty_supplies_store.Models;
+namespace api.Models;
 
 [Table("products")]
 public class Product
