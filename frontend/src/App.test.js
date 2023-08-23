a50e3ce4bc6853cf4edfbcbda7edecef149a8147
sszymiczek <szymonszymiczek32@gmail.com>
2023-08-23 17:51:48 +0200

    removing react base content

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// This test has no actual use, it's only for future reference.
+// Should be deleted after actual tests are made.
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
