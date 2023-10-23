84b53ffd602134b5238b876f9d197580a3b48661
sszymiczek <56030608+sszymiczek@users.noreply.github.com>
2023-10-23 19:00:29 +0200

    Bss 4 frontend create basic template of frontend to display something in browser (#10)
    * simple dockerised react project
    
    * making hot reload
    
    * removing react base content
    
    * ports chenged according to convention
    
    * ports chenged according to convention
    
    fix
    
    * resolving PR comments
    
    * removed unnecessary dockerfile
    
    * cleared index.html

---
+++ frontend/src/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+// This test has no actual use, it's only for future reference.
+// Should be deleted after actual tests are made.
+// test('example test', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/example element/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
